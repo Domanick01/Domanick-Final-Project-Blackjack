4ac878e5e95d7b0cca424907ce46d1f5fcd9970c
Domanick01 <domanick731@gmail.com>
2024-10-31 12:10:52 -0400

    defined the main game function and setup a basic random card system

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,32 @@
 # blackjack.py
 # This program is for my final project and will be a close depiction to the
 # card game blackjack
+
+
+import random
+
+def mainGame():
+    cards = ["King", "Queen", "Jack", 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    playerCards = []
+    dealerCards = []
+    money = 0
+
+    def pullCard():
+        x = random.choices(cards, k = 1)
+        return x
+    
+    playGame = input("Welcome To Python Blackjack, Would you like to play (yes or no) >> ").lower()
+    if playGame[0] == "y":
+        if playerCards == []:
+            playerCards += pullCard()
+            dealerCards += pullCard()
+    elif playGame[0] == "n":
+        exit()
+
+    
+
+
+
+
+if __name__ == "__main__":
+    mainGame()    
