fd065f6bb3f8137cb37c9ef033d3373099a08e94
Domanick01 <domanick731@gmail.com>
2024-11-01 13:50:15 -0400

    changed finding player and dealer total with function as well as working on basic blackjack functions

--- blackjack.py
+++ blackjack.py
@@ -9,6 +9,7 @@ def mainGame():
     # variables
     cards = ["King", "Queen", "Jack", "Ace", 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     playerCards = []
+    amount = 0
     playerTotal = 0
     dealerCards = []
     dealerTotal = 0
@@ -19,39 +20,47 @@ def mainGame():
         x = random.choices(cards, k = 1)
         return x
     
+    # function for defining face value cards
+    def faceValues(x):
+        if x == "King":
+            return 10
+        elif x == "Queen":
+            return 10
+        elif x == "Jack":
+            return 10
+        elif x == "Ace":
+            return 11
+        else:
+            return x
+    
     playGame = input("Welcome To Python Blackjack, Would you like to play (yes or no) >> ").lower()
     if playGame[0] == "y":
         if playerCards == []:
             for i in range(2):
                 playerCards += pullCard()
+                amount += 1
                 dealerCards += pullCard()
                 
             # if statements for calculating total for both player and dealer
             for i in playerCards:
-                if i == "King":
-                    playerTotal += 10
-                elif i == "Queen":
-                    playerTotal += 10
-                elif i == "Jack":
-                    playerTotal += 10
-                elif i == "Ace":
-                    playerTotal += 11
-                else:
-                    playerTotal += i
+                playerTotal += faceValues(i)
             for i in dealerCards:
-                if i == "King":
-                    dealerTotal += 10
-                elif i == "Queen":
-                    dealerTotal += 10
-                elif i == "Jack":
-                    dealerTotal += 10
-                elif i == "Ace":
-                    dealerTotal += 11
-                else:
-                    dealerTotal += i
+                dealerTotal += faceValues(i)
+
+        print(f"Player's Cards: {playerCards} Total: {playerTotal} {"\n"}Dealer's Cards: {dealerCards} Total: {dealerTotal}")
+        
+        while playerTotal and dealerTotal <= 21:
+            nextMove = input("What is your next move (hit, stand, double) >> ").lower
+            if nextMove == "hit":
+                print("NEXT MOVE HAS RAN")
+                playerCards += pullCard()
+                playerTotal += faceValues(playerCards[amount])
+                print(f"Player's Cards: {playerCards} Total: {playerTotal} {"\n"}Dealer's Cards: {dealerCards} Total: {dealerTotal}")
 
     elif playGame[0] == "n":
         exit()
+    else:
+        mainGame()
 
     print(f"Player's Cards: {playerCards} Total: {playerTotal} {"\n"}Dealer's Cards: {dealerCards} Total: {dealerTotal}")
     
